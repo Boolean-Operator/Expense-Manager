9e48a20dd199690d9a16f8d022be3828df7f69eb
Boolean-Operator <marktgraybill@gmail.com>
2019-02-25 09:05:44 -0500

    Correct typo

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Bean Counter</h1>  
-      <p>It's time to get your finances under control.</p>
+      <p>It is time to get your finances under control.</p>
       <button className="button" onClick={startLogin}>
         Log In with Google
       </button>
